7a17dd3372243d9cf13fe7e1e94db6b6e0574cab
clint <cgilbert@broadinstitute.org>
2018-07-27 17:42:43 -0400

    Add a dummy config.json to src/test/resources so one isn't needed in the project root to run the unit tests

--- src/test/scala/org/broadinstitute/dig/aggregator/core/OptsTest.scala
+++ src/test/scala/org/broadinstitute/dig/aggregator/core/OptsTest.scala
@@ -9,27 +9,31 @@ import org.rogach.scallop.exceptions.ScallopException
  */
 final class OptsTest extends FunSuite {
   
-  private def opts(commandLine: String): Opts[Config] = new Opts(commandLine.split("\\s+"))
+  private def opts(commandLine: String): Opts[Config] = {
+    new Opts(commandLine.split("\\s+"))
+  }
+  
+  private val confFile = "src/test/resources/config.json"
   
   test("--from-beginning and --continue are mutually exclusive") {
     intercept[ScallopException] {
-      opts("--from-beginning --continue")
+      opts(s"--from-beginning --continue --config $confFile")
     }
     
     intercept[ScallopException] {
-      opts("--continue --from-beginning")
+      opts("--continue --from-beginning --config $confFile")
     }
     
     //one at a time is fine
     
-    opts("--from-beginning")
+    opts(s"--from-beginning --config $confFile")
     
-    opts("--continue")
+    opts(s"--continue --config $confFile")
   }
   
   test("position") {
-    assert(opts(" ").position == State.End)
-    assert(opts("--from-beginning").position == State.Beginning)
-    assert(opts("--continue").position == State.Continue)
+    assert(opts(s"--config $confFile").position == State.End)
+    assert(opts(s"--from-beginning --config $confFile").position == State.Beginning)
+    assert(opts(s"--continue --config $confFile").position == State.Continue)
   }
 }
