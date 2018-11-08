b9988c7eff83d493c26d3f8597ec984918ca2945
Jeffrey Massung <massung@gmail.com>
2018-11-08 15:06:37 -0600

    Added JSON serialization for AmiId.

--- src/main/scala/org/broadinstitute/dig/aggregator/core/emr/AmiId.scala
+++ src/main/scala/org/broadinstitute/dig/aggregator/core/emr/AmiId.scala
@@ -1,5 +1,7 @@
 package org.broadinstitute.dig.aggregator.core.emr
 
+import org.json4s._
+
 /**
  * When creating an EMR cluster, you provide the AMI (EC2 image instance)
  * to clone for the master and slave nodes.
@@ -15,4 +17,22 @@ object AmiId {
 
   /** Constant AMI provided by AWS with nothing special. */
   val amazonLinux_2018_3: AmiId = AmiId("ami-f316478c")
+
+  /** Convert a JSON value to an AmiId. */
+  val deserialize: PartialFunction[JValue, AmiId] = {
+    case JString(value) => AmiId(value)
+  }
+
+  /** Convert an AmiId to a JSON value. */
+  val serialize: PartialFunction[Any, JValue] = {
+    case AmiId(value) => JString(value)
+  }
+
+  /**
+   * Custom serializer for AmiId. To use this, add it to the default
+   * formats when deserializing...
+   *
+   * implicit val formats = json4s.DefaultFormats + AmiId.Serializer
+   */
+  case object Serializer extends CustomSerializer[AmiId](format => deserialize -> serialize)
 }
