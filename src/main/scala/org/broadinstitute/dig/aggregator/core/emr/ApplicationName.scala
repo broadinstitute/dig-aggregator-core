4fe86a4f11455ecaf2b4a384b6f124ff293c9b2f
Jeffrey Massung <massung@gmail.com>
2018-11-15 12:10:39 -0600

    Sealed to final.

--- src/main/scala/org/broadinstitute/dig/aggregator/core/emr/ApplicationName.scala
+++ src/main/scala/org/broadinstitute/dig/aggregator/core/emr/ApplicationName.scala
@@ -3,7 +3,7 @@ package org.broadinstitute.dig.aggregator.core.emr
 import com.amazonaws.services.elasticmapreduce.model.Application
 
 /** An application that can be pre-installed on the cluster. */
-sealed case class ApplicationName(value: String) {
+final case class ApplicationName(value: String) {
 
   /** Create the EMR Application object. */
   def application: Application = new Application().withName(value)
