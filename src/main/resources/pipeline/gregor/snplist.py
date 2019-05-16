0bccb021002577ab7bc8f1d7a1c1785023cb5f93
Jeffrey Massung <massung@gmail.com>
2019-05-15 19:06:45 -0500

    Improved (results) SNP list for GREGOR.

--- src/main/resources/pipeline/gregor/snplist.py
+++ src/main/resources/pipeline/gregor/snplist.py
@@ -4,8 +4,8 @@ import argparse
 import platform
 import subprocess
 
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, concat_ws, input_file_name, regexp_extract  # pylint: disable=E0611
+from pyspark.sql import SparkSession, Row
+from pyspark.sql.functions import col, input_file_name, regexp_extract  # pylint: disable=E0611
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DoubleType
 
 s3dir = 's3://dig-analysis-data'
@@ -14,7 +14,7 @@ s3dir = 's3://dig-analysis-data'
 valid_chromosomes = list(map(lambda c: str(c+1), range(22))) + ['X', 'Y']
 
 # output schema of meta-analysis
-variants_schema = StructType(
+meta_analysis_schema = StructType(
     [
         StructField('varId', StringType(), nullable=False),
         StructField('chromosome', StringType(), nullable=False),
@@ -63,40 +63,74 @@ if __name__ == '__main__':
 
     spark = SparkSession.builder.appName('gregor').getOrCreate()
 
+    # The algorithm here is to find all the best SNPs on each chromosome, but only
+    # SNPs within a given range of each other (e.g. 500 kb). The following
+    # algorithm is used:
+    #
+    #   1. Create an empty DataFrame of SNPs that will serve as the output
+    #   2. For each chromosome:
+    #      a. find all SNPs < p-value threshold (source DF)
+    #      b. find the best SNP (lowest p-value) a put it into the output DF
+    #      c. remove all SNPs within the given range of (b) from the source DF
+    #      d. if the source DF is not empty, goto 2b.
+    #   3. The output DF now contains all the SNPs to use.
+
+    output_schema = StructType([
+        StructField('SNP', StringType()),
+        StructField('ancestry', StringType()),
+    ])
+
+    # create the output SNP data frame
+    output_SNPs = []
+
     # NOTE: It's possible that srcdir doesn't actually exist or contain anything!
     #       This is because meta-analysis filters data. For example, perhaps a
     #       dataset doesn't contain any BETA or P-VALUE data, in which case it
     #       will be successfully run through the meta-analysis processor, but it
     #       won't write anything out or get through ancestry-specific analysis.
     #
-    #       When that happens, it's OK to just ignore it.
+    #       When that happens, it's OK to just have an empty output SNP list.
 
     if test_glob(srcdir):
-        df = spark.read.csv(srcdir, sep='\t', header=True, schema=variants_schema) \
+        df = spark.read.csv(srcdir, sep='\t', header=True, schema=meta_analysis_schema) \
             .withColumn('filename', input_file_name()) \
             .withColumn('ancestry', regexp_extract('filename', r'/ancestry=([^/]+)/', 1)) \
             .filter(col('chromosome').isin(valid_chromosomes)) \
-            .filter(col('pValue') < 1.0e-5) \
-            .rdd \
-            .keyBy(lambda v: (v.ancestry, v.chromosome, v.position // 500000)) \
-            .reduceByKey(lambda a, b: b if b.pValue < a.pValue else a) \
-            .map(lambda v: v[1]) \
-            .toDF() \
-            .select(
-                concat_ws(':', col('chromosome'), col('position')).alias('SNP'),
-                col('ancestry'),
-            )
-    else:
-        schema = StructType([
-            StructField('SNP', StringType()),
-            StructField('ancestry', StringType()),
-        ])
+            .filter(col('pValue') < 1.0e-5)
 
-        # so _SUCCESS is written
-        df = spark.createDataFrame([], schema)
+        # get a distinct list of each ancestries
+        ancestries = df.select(df.ancestry) \
+            .distinct() \
+            .collect()
+
+        # loop over each chromosome and ancestry to get the SNPs for it
+        for chromosome in valid_chromosomes:
+            for a_row in ancestries:
+                ancestry = a_row.ancestry
+
+                # Collect all the SNPs on this chromosome for this ancestry.
+                #
+                # Sort all the SNPs by p-value so the lowest p-value is last. This improves
+                # performance considerably over using `min()` each iteration as we can just
+                # grab the last SNP in the list, then `filter()`, which is stable.
+                source_SNPs = df.filter((df.chromosome == chromosome) & (df.ancestry == ancestry)) \
+                    .select(df.chromosome, df.position, df.pValue) \
+                    .sort(df.pValue.desc()) \
+                    .collect()
+
+                # find the lowest p-value SNP
+                while len(source_SNPs) > 0:
+                    best = source_SNPs.pop()
+
+                    # add the best SNP to the list of output SNPs
+                    output_SNPs.append(Row(SNP='%s:%d' % (best.chromosome, best.position), ancestry=ancestry))
+
+                    # remove all SNPs from the source list within a given range
+                    source_SNPs = list(filter(lambda row: abs(row.position - best.position) > 500000, source_SNPs))
 
     # output the variants as CSV part files
-    df.write \
+    spark.createDataFrame(output_SNPs, output_schema) \
+        .write \
         .mode('overwrite') \
         .partitionBy('ancestry') \
         .csv(outdir, sep='\t')
