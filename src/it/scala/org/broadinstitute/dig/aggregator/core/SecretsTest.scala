7dc4abd805a4653ee9791488958ecca71586ba95
clint <cgilbert@broadinstitute.org>
2019-09-03 17:18:49 -0400

    Added SecretsTest, containing cases taken from the now-factored-out AwsTest

---
+++ src/it/scala/org/broadinstitute/dig/aggregator/core/SecretsTest.scala
@@ -0,0 +1,20 @@
+package org.broadinstitute.dig.aggregator.core
+
+import org.scalatest.FunSuite
+import org.broadinstitute.dig.aggregator.core.config.Secrets
+
+/**
+ * @author jmassung
+ */
+final class SecretsTest extends FunSuite {
+  test("secrets") {
+    val secret = Secrets.get[SecretsTest.TestSecret]("hello")
+
+    assert(secret.isSuccess)
+    assert(secret.get.hello == "world")
+  }
+}
+
+object SecretsTest {
+  final case class TestSecret(hello: String)
+}
