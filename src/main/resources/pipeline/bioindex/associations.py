cc307aca09555cfdf3a3d03f529809f3491f0562
Jeffrey Massung <massung@gmail.com>
2020-04-21 09:52:22 -0500

    BioIndex processor updates.

--- src/main/resources/pipeline/bioindex/associations.py
+++ src/main/resources/pipeline/bioindex/associations.py
@@ -7,7 +7,8 @@ srcdir = 's3://dig-analysis-data/out/metaanalysis/trans-ethnic/*/part-*'
 outdir = 's3://dig-bio-index/associations'
 
 # common vep data
-vepdir = 's3://dig-analysis-data/out/varianteffect/common/part-*'
+common_dir = 's3://dig-analysis-data/out/varianteffect/common'
+genes_dir = 's3://dig-analysis-data/genes/GRCh37'
 
 # this is the schema written out by the variant partition process
 variants_schema = StructType(
@@ -33,39 +34,44 @@ if __name__ == '__main__':
 
     # load the trans-ethnic, meta-analysis, top variants and write them sorted
     df = spark.read.csv(srcdir, sep='\t', header=True, schema=variants_schema)
-    common = spark.read.json(vepdir)
+    common = spark.read.csv('%s/part-*' % common_dir, sep='\t', header=True)
+    genes = spark.read.json(genes_dir)
 
-    # join the dbSNP id with the associations
+    # join the common data into the associations
     df = df.join(common, 'varId', how='left_outer')
-    top = df.filter(df.top)
 
-    # find the variants most significant across the genome per phenotype, this
-    # assumes a manhattan plot 4000 pixels wide, which means a maximum of 4000
-    # "positions" to display, so 3B positions / 4000 = 750,000 to keep the best
-    # variant in (or significant ones)
-    by_phenotype = df.rdd \
-        .keyBy(lambda v: (v.phenotype, v.chromosome, v.position // 750000)) \
-        .reduceByKey(lambda a, b: b if b.pValue < a.pValue else a) \
-        .map(lambda v: v[1]) \
-        .toDF() \
-        .union(df.filter(df.pValue < 1e-5))
-
-    # all associations indexed by locus
+    # write associations indexed by phenotype and locus
     df.drop('top') \
         .orderBy(['phenotype', 'chromosome', 'position']) \
         .write \
         .mode('overwrite') \
         .json('%s/locus' % outdir)
 
-    # top associations indexed by locus
-    top.drop('top') \
+    # write out just the top associations, indexed by locus
+    df.filter(df.top) \
+        .drop('top') \
         .orderBy(['chromosome', 'position']) \
         .write \
         .mode('overwrite') \
         .json('%s/top' % outdir)
 
-    # write out the genome-wide associations by phenotype
-    by_phenotype.drop(['rank', 'top']) \
+    # Find all the variants (per phenotype) across the genome that will be
+    # used for manhattan plots, and finding variants.
+    #
+    # This is done similar to the "top" associations: find the most significant
+    # variant within a range of each chromosome and only keep those. However,
+    # we also keep all associations with signal as well.
+    #
+    # With the "top" associations, this is done using a small range (~5 kb),
+    # while this will be done with a very large range.
+
+    df.filter(df.pValue > 1.e-5) \
+        .rdd \
+        .keyBy(lambda v: (v.phenotype, v.chromosome, v.position // 250000)) \
+        .reduceByKey(lambda a, b: b if b.pValue < a.pValue else a) \
+        .map(lambda v: v[1]) \
+        .toDF() \
+        .union(df.filter(df.pValue < 1.e-5)) \
         .orderBy(['phenotype', 'pValue']) \
         .write \
         .mode('overwrite') \
